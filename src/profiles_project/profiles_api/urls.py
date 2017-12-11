bb0ac21b2208e9c4ab1ce66ae42b747bf84fd1a5
Ross Anderson <rosscoryanderson@gmail.com>
2017-12-11 21:53:09 +0800

    Added login authentication API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -7,7 +7,9 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewset, base_name='hello-viewset')
+# dont need a base_name with ModelViewset
 router.register('profile', views.UserProfileViewset) 
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
